a4576f6d264b4874553e379077d3563c7a3d303e
raptao <thierryrabearijao@gmail.com>
2016-11-30 22:45:00 +0100

    #build-configuration deploying in glassfish is possible
    hello world page done

---
+++ src/main/java/fr/upem/jee/allodoc/WelcomeBean.java
@@ -0,0 +1,13 @@
+package fr.upem.jee.allodoc;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name = "welcome", eager = true)
+public class WelcomeBean {
+    public WelcomeBean() {
+        System.out.println("WelcomeBean instantiated");
+    }
+    public String getMessage() {
+        return "I'm alive!";
+    }
+}
