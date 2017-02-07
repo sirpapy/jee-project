77ef638b5bd4ee7d244dae3bbb2217a0729d76ba
Thierry RABEARIJAO <thierryrabearijao@gmail.com>
2017-02-07 10:26:31 +0100

    #authentication done - ConnectedUserBean not completed

--- src/main/java/fr/upem/jee/allodoc/faces/ConnectedUserBean.java
+++ src/main/java/fr/upem/jee/allodoc/faces/ConnectedUserBean.java
@@ -1,7 +1,10 @@
 package fr.upem.jee.allodoc.faces;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by raptao on 1/21/2017.
@@ -13,6 +16,11 @@ public class ConnectedUserBean {
     private String connectedUsername;
 
     public String getConnectedUsername() {
+        if( connectedUsername == null ){
+        HttpServletRequest req = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+        String name = req.getUserPrincipal().getName();
+            connectedUsername = name;
+        }
         return connectedUsername;
     }
 
@@ -20,8 +28,7 @@ public class ConnectedUserBean {
         this.connectedUsername = connectedUsername;
     }
 
-    public String load() {
-
-        return "";
+    @PostConstruct
+    public void load() {
     }
 }
