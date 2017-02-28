d64a6f5dcc464bf3a3897b0594ed6060e0e594d7
Thierry RABEARIJAO <thierryrabearijao@gmail.com>
2017-02-28 19:28:08 +0100

    #version 1 added postal code converter

---
+++ src/main/java/fr/upem/jee/allodoc/faces/converter/PostalCodeConverter.java
@@ -0,0 +1,38 @@
+package fr.upem.jee.allodoc.faces.converter;
+
+import com.google.common.base.Preconditions;
+import fr.upem.jee.allodoc.entity.Location;
+import fr.upem.jee.allodoc.service.LocationService;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+import java.util.Optional;
+
+/**
+ * Created by raptao on 2/28/2017.
+ */
+@FacesConverter("postalCodeConverter")
+public class PostalCodeConverter implements Converter {
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+        Preconditions.checkNotNull(value, "postalCodeString should not be null");
+        LocationService locationService = new LocationService();
+        int postalCode = Integer.parseInt(value);
+        Optional<Location> byPostalCode = locationService.getByPostalCode(postalCode);
+        if (byPostalCode.isPresent()) {
+            return byPostalCode.get();
+        }
+        Location location = new Location.Builder().setPostalCode(postalCode).build();
+        locationService.save(location);
+        return location;
+    }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+        Preconditions.checkNotNull(value, "postalCode should not be null");
+        Location asLocation = (Location)value;
+        return asLocation.getPostalCode().toString();
+    }
+}
