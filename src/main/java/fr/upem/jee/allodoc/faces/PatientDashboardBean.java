83749549c58f8dd2f408d938be36f4951026924a
Thierry RABEARIJAO <thierryrabearijao@gmail.com>
2017-03-07 23:20:01 +0100

    #version2 updates on patient home page

--- src/main/java/fr/upem/jee/allodoc/faces/PatientDashboardBean.java
+++ src/main/java/fr/upem/jee/allodoc/faces/PatientDashboardBean.java
@@ -66,7 +66,6 @@ public class PatientDashboardBean {
         Preconditions.checkArgument(selectedPhysicianAfterSearch > 0, "You didn't selected a physician");
         PhysicianService physicianService = new PhysicianService();
         getSelectedPhysician = physicianService.findByLongId(Physician.class, selectedPhysicianAfterSearch);
-
     }
 
     public int getSelectedPhysicianAfterSearch() {
